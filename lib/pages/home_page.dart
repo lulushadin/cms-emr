3e6fac4d56213199b173e94449fc12b04fff200f
lulushadin <20mcs21@meaec.edu.in>
2024-12-31 11:59:04 +0530

    color code added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,9 +8,34 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
+        title: Text(
           'EMR',
-          style: TextStyle(fontSize: 19),
+          style: GoogleFonts.roboto(
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 12, top: 8, right: 12, bottom: 8),
+        child: Column(
+          children: [
+            Text(
+              'Electronic Medical Records ',
+              style:
+                  GoogleFonts.roboto(fontWeight: FontWeight.w500, fontSize: 14),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 155,
+              width: 109,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            )
+          ],
         ),
       ),
     );
---
+++ lib/utils/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const colorwhite = Colors.white,
+    colorblack = Colors.black87,
+    primarycolor = Color.fromRGBO(1, 191, 225, 1),
+    secondarycolor = Color.fromRGBO(237, 252, 255, 1);
